80d615009a6200fc1b1cdeec21929c243cd489b9
kibet-wilfred <wikiearapkoech@gmail.com>
2024-03-11 13:50:30 +0300

    second commit

---
+++ wilfred python.py
@@ -0,0 +1,33 @@
+# python comment -single line
+"""
+multi-line comment
+"""
+
+#variables
+student_name ="Wilfred" #data type is string
+student_age = 20 #data type is integer
+student_fee = 100.0 #data type is float
+student_marks = 100
+is_male = True #data type is boolean
+
+#outputting the values in the variables
+print(student_name)
+print(student_age)
+print(student_fee)
+print(is_male)
+
+
+
+
+#Case sensitivity
+student_name="kibe"
+STUDENT_NAME="koech"
+
+
+#variable naming in python
+book_price =20
+_book_price =20
+book_price_123=20
+
+x=y=z=10 #one value to many variables
+x,y,z=10,20,30 #multiple value
