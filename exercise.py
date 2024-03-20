772a8616a65f81ab19b1195ca1ac8830c84f5e36
kibet-wilfred <wikiearapkoech@gmail.com>
2024-03-20 13:41:30 +0300

    addings

--- exercise.py
+++ exercise.py
@@ -16,7 +16,7 @@ Bucket_list = {
     "Friday":"skating"
     }
 print(Bucket_list)
-del Bucket_list["Monday"]
+del Bucket_list["Monday"],Bucket_list[ "Wednesday"]
 print(Bucket_list)
 x = Bucket_list.get("Thursday")
 print(x)
@@ -43,3 +43,23 @@ print(fruits)
 
 fruits.remove("orange")
 print(fruits)
+
+
+countries = ["kenya","uganda","france","germany","kenya","france"]
+def repeated_countries(countries):
+    for country in countries:
+        if country == country:
+            return country
+print("The repeated country is:",repeated_countries(countries))
+
+fruits = ["banana", "apple", "banana","apple" ,"banana", "mango"]
+repeated_fruits = set()
+unrepeated_fruits = set()
+for fruit in fruits:
+    if fruits.count(fruit)>1:
+        repeated_fruits.add(fruit)
+
+    else:
+        unrepeated_fruits.add(fruit)
+print("the repeated fruits are:", repeated_fruits)
+print("the unrepeated fruits", unrepeated_fruits)
